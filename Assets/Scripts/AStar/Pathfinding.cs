2f72601894b3e0a5da2926e9b7c030a6f7a71bc7
codestellations <erlindaargyt@gmail.com>
2020-03-24 23:06:06 +0700

    Updated pathfinding

---
+++ Assets/Scripts/AStar/Pathfinding.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Pathfinding : MonoBehaviour
+{
+    PathRequestManager requestManager;
+    // public Transform seeker, target;
+    Grid grid;
+
+    // void Update(){
+    //     FindPath(seeker.position, target.position);
+    // }
+
+    void Awake(){
+        requestManager = GetComponent<PathRequestManager>();
+        grid = GetComponent<Grid>();
+    }
+
+    public void StartFindPath(Vector3 startPos, Vector3 targetPos){
+        StartCoroutine(FindPath(startPos, targetPos));
+    }
+    IEnumerator FindPath(Vector3 startPos, Vector3 targetPos){
+        Vector3[] waypoints = new Vector3[0];
+        bool pathSuccess = false;
+
+        Node startNode = grid.NodeFromWorldPoint(startPos);
+        Node targetNode = grid.NodeFromWorldPoint(targetPos);
+
+        if (startNode.walkable && targetNode.walkable){
+            List<Node> openSet = new List<Node>();
+            HashSet<Node> closedSet = new HashSet<Node>();
+            openSet.Add(startNode);
+
+            while (openSet.Count > 0){
+                Node currentNode = openSet[0];
+                for (int i = 1; i < openSet.Count; i++){
+                    if(openSet[i].fCost < currentNode.fCost || openSet[i].fCost == currentNode.fCost && openSet[i].hCost < currentNode.hCost){
+                        currentNode = openSet[i];
+                    }
+                }
+
+                openSet.Remove(currentNode);
+                closedSet.Add(currentNode);
+
+                if (currentNode == targetNode){
+                    pathSuccess = true;
+                    break;
+                }
+
+                foreach (Node neighbor in grid.GetNeighbors(currentNode)){
+                    if (!neighbor.walkable || closedSet.Contains(neighbor)){
+                        continue;
+                    }
+
+                    int newMovementCostToNeighbor = currentNode.gCost + GetDistance(currentNode, neighbor);
+                    if (newMovementCostToNeighbor < neighbor.gCost || !openSet.Contains(neighbor)){
+                        neighbor.gCost = newMovementCostToNeighbor;
+                        neighbor.hCost = GetDistance(neighbor, targetNode);
+                        neighbor.parent = currentNode;
+
+                        if (!openSet.Contains(neighbor)){
+                            openSet.Add(neighbor);
+                        }
+                    }
+                }
+            }
+        }
+        yield return null;
+        if (pathSuccess){
+            waypoints = RetracePath(startNode, targetNode);
+        }
+        requestManager.FinishedProcessingPath(waypoints, pathSuccess);
+    }
+
+    Vector3[] RetracePath(Node startNode, Node endNode){
+        List<Node> path = new List<Node>();
+        Node currentNode = endNode;
+
+        while (currentNode != startNode){
+            path.Add(currentNode);
+            currentNode = currentNode.parent;
+        }
+        Vector3[] waypoints = SimplifyPath(path);
+        Array.Reverse(waypoints);
+        path.Reverse();
+
+        grid.path = path;
+
+        return waypoints;
+    }
+
+    Vector3[] SimplifyPath(List<Node> path){
+        List<Vector3> waypoints = new List<Vector3>();
+        Vector2 directionOld = Vector2.zero;
+
+        for (int i = 1; i < path.Count; i++){
+            Vector2 directionNew = new Vector2(path[i-1].gridX - path[i].gridX, path[i-1].gridY - path[i].gridY);
+            if (directionNew != directionOld){
+                waypoints.Add(path[i].worldPosition);
+            }
+            directionOld = directionNew;
+        }
+        return waypoints.ToArray();
+    }
+
+    int GetDistance(Node nodeA, Node nodeB){
+        int distX = Mathf.Abs(nodeA.gridX - nodeB.gridX);
+        int distY = Mathf.Abs(nodeA.gridY - nodeB.gridY);
+
+        if (distX > distY){
+            return 14*distY + 10*(distX-distY);
+        }
+
+        return 14*distX + 10*(distY-distX);
+    }
+}
